e8183a018ce60f520934b058cec91b4d468f5e12
B10213147 <B10213147@yuntech.org.tw>
2016-03-05 14:29:56 +0100

    0.2

--- lab1/main.c
+++ lab1/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define false 0
 #define true 1
+#define empty -1
 
 typedef struct{
     float real_part;
@@ -11,8 +13,14 @@ typedef struct{
     float phase;
 }complex_num;
 
-int main()
-{
+int char2int(char in){
+    int out = in - 48;
+    printf("out=%d\n",out);
+    return out;
+}
+
+complex_num get_Complex(void){
+    complex_num temp;
     char str[10];
     gets(str);
 
@@ -22,33 +30,37 @@ int main()
     int i = 0;
     char mid_Sign;
     char first_Sign;
+    int last_Sign_bit;
+    int mid_Sign_bit;
 
-    while(str[i] != NULL) i++;
+    while(str[i] != 0) i++;  //total bits of input string
 
     printf("%d\n",i);
 
-    if(str[i-1] == 'i'){
+    if(str[i-1] == 'i'){    //find the last bit is 'i' or not
         last_flag = true;
     }
     else{
         last_flag = false;
     }
+    last_Sign_bit = i-1;
     i--;
 
+    //find mid_sign is at which bit
     while(str[i-1] >= 48 && str[i-1] <= 57){
         i--;
     }
 
-    if(i-1 <= 0){
+    if(i-1 <= 0){   //check mid_flag is not at the first bit
         mid_flag = false;
-        mid_Sign = NULL;
+        mid_Sign = empty;
+        mid_Sign_bit = empty;
     }
     else{
         mid_flag = true;
         mid_Sign = str[i-1];
+        mid_Sign_bit = i-1;
     }
-
-    //printf("mid_sign=%c\n", str[i-1]);
 
     if(str[0] == '-'){   //check first character is negative or not
         first_flag = true;
@@ -56,14 +68,23 @@ int main()
     }
     else{
         first_flag = false;
-        first_Sign = NULL;
+        first_Sign = empty;
     }
 
     int check = (int)first_flag*100 + (int)mid_flag*10 + (int)last_flag;
+    float sum = 0;
+    int j;
 
     switch(check){
     case 0:
         printf("only real part\n");
+        int n = 0;
+        for(j=last_Sign_bit; j>=0; j--){
+            sum = sum + char2int(str[j]) * pow(10,n);
+            printf("sum=%f\n",sum);
+            n++;
+        }
+        temp.real_part = sum;
         break;
     case 1:
         printf("only imaginary part\n");
@@ -91,6 +112,16 @@ int main()
 
     }
     printf("check=%d\n",check);
+    return temp;
+}
+
+int main()
+{
+    complex_num a, b;
+    a = get_Complex();
+    printf("real_part=%d\n",(int)a.real_part);
+    //b = get_Complex();
+    //printf("real_part=%d\n",b.real_part);
 
     return 0;
 }
